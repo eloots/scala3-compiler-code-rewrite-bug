08bfebae9e78409e8cb168fac00dd217f953f11a
Eric Loots <eric.loots@gmail.com>
2023-03-30 17:46:22 +0200

    Code after rewriting to noindent based syntax

--- src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuIO.scala
+++ src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuIO.scala
@@ -3,21 +3,24 @@ package org.lunatechlabs.dotty.sudoku
 import java.io.{BufferedReader, File, FileReader}
 import java.util.NoSuchElementException
 
-object SudokuIO:
+object SudokuIO {
 
   private def sudokuCellRepresentation(content: CellContent): String =
-    content.toList match
+    content.toList match {
       case Nil                => "x"
       case singleValue +: Nil => singleValue.toString
       case _                  => " "
+    }
 
-  private def sudokuRowPrinter(threeRows: Vector[ReductionSet]): String =
-    val rowSubBlocks = for
+  private def sudokuRowPrinter(threeRows: Vector[ReductionSet]): String = {
+    val rowSubBlocks = for {
       row <- threeRows
       rowSubBlock <- row.map(el => sudokuCellRepresentation(el)).sliding(3, 3)
       rPres = rowSubBlock.mkString
+    }
     yield rPres
     rowSubBlocks.sliding(3, 3).map(_.mkString("", "|", "")).mkString("|", "|\n|", "|\n")
+  }
 
   def sudokuPrinter(result: SudokuSolver.SudokuSolution): String =
     result.sudoku.sliding(3, 3).map(sudokuRowPrinter).mkString("\n+---+---+---+\n", "+---+---+---+\n", "+---+---+---+")
@@ -26,53 +29,62 @@ object SudokuIO:
    * FileLineTraversable code taken from "Scala in Depth" by Joshua Suereth
    */
 
-  class FileLineTraversable(file: File) extends Iterable[String]:
+  class FileLineTraversable(file: File) extends Iterable[String] {
     val fr = new FileReader(file)
     val input = new BufferedReader(fr)
     var cachedLine: Option[String] = None
     var finished: Boolean = false
 
-    override def iterator: Iterator[String] = new Iterator[String]:
+    override def iterator: Iterator[String] = new Iterator[String] {
 
-      override def hasNext: Boolean = (cachedLine, finished) match
+      override def hasNext: Boolean = (cachedLine, finished) match {
         case (Some(_), _) => true
 
         case (None, true) => false
 
         case (None, false) =>
-          try
+          try {
             val line = input.readLine()
-            if line == null then
+            if line == null then {
               finished = true
               input.close()
               fr.close()
               false
-            else
+            }
+            else {
               cachedLine = Some(line)
               true
-          catch
+            }
+          }
+          catch {
             case e: java.io.IOError =>
               throw new IllegalStateException(e.toString)
+          }
+      }
 
-      override def next(): String =
+      override def next(): String = {
         if !hasNext then throw new NoSuchElementException("No more lines in file")
         val currentLine = cachedLine.get
         cachedLine = None
         currentLine
+      }
+    }
     override def toString: String =
       "{Lines of " + file.getAbsolutePath + "}"
+  }
 
   def convertFromCellsToComplete(cellsIn: Vector[(String, Int)]): Vector[(Int, CellUpdates)] =
-    for
+    for {
       (rowCells, row) <- cellsIn
       updates = rowCells.zipWithIndex.foldLeft(cellUpdatesEmpty) {
         case (cellUpdates, (c, index)) if c != ' ' =>
           (index, Set(c.toString.toInt)) +: cellUpdates
         case (cellUpdates, _) => cellUpdates
       }
+    }
     yield (row, updates)
 
-  def readSudokuFromFile(sudokuInputFile: java.io.File): Vector[(Int, CellUpdates)] =
+  def readSudokuFromFile(sudokuInputFile: java.io.File): Vector[(Int, CellUpdates)] = {
     val dataLines = new FileLineTraversable(sudokuInputFile).toVector
     val cellsIn =
       dataLines
@@ -83,3 +95,5 @@ object SudokuIO:
         .zipWithIndex
 
     convertFromCellsToComplete(cellsIn)
+  }
+}
